c20a9bdebd6e64d9f95f543abd1a9c6324637f01
Simone Carriero <simonecarriero@gmail.com>
2022-10-24 23:32:31 +0200

    Execute sell

--- src/wallet.rs
+++ src/wallet.rs
@@ -12,6 +12,7 @@ impl Wallet {
         for transaction in &operation.0 {
             match transaction {
                 Transaction::Buy { quantity } => self.buy(*quantity)?,
+                Transaction::Sell { quantity } => self.sell(*quantity)?,
             }
         }
         Ok(self.quantity)
@@ -19,6 +20,11 @@ impl Wallet {
 
     fn buy(&mut self, quantity: u16) -> Result<(), anyhow::Error> {
         self.quantity += quantity as i16;
+        Ok(())
+    }
+
+    fn sell(&mut self, quantity: u16) -> Result<(), anyhow::Error> {
+        self.quantity -= quantity as i16;
         Ok(())
     }
 }
@@ -29,6 +35,7 @@ pub struct Operation(Vec<Transaction>);
 #[derive(Clone, Debug)]
 pub enum Transaction {
     Buy { quantity: u16 },
+    Sell { quantity: u16 },
 }
 
 #[cfg(test)]
@@ -44,5 +51,19 @@ mod tests {
 
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), 7);
+    }
+
+    #[test]
+    fn buy_and_sell_some_stock() {
+        let mut wallet = Wallet::new();
+        let operation = Operation(vec![
+            Transaction::Buy { quantity: 7 },
+            Transaction::Sell { quantity: 2 },
+        ]);
+
+        let result = wallet.execute(&operation);
+
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 5);
     }
 }
